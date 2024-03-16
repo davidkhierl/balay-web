a7f3ce0f5aa63ec8b883797bccc217fca0165976
David Khierl <dkrgidor@gmail.com>
2024-03-16 22:17:33 +0400

    Refactor user authentication in HomePage
    Deleted the Home function file and replaced it with the HomePage function file. The HomePage function provides a cleaner, more efficient user authentication service by fetching the current user and their households. The refactor aims to improve the structure and readability of the code.

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,25 @@
+import { HouseholdInitialCreate } from '@/components/households/household-initial-create'
+import { getCurrentUser } from '@/lib/services/auth/get-current-user'
+import { userHouseholds } from '@/lib/services/household/user-households'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function HomePage() {
+  const supabase = createClient()
+  const user = await getCurrentUser(supabase)
+  const { data: households, count } = await userHouseholds(user.id, supabase)
+
+  if (!households?.length)
+    return (
+      <div className="container flex min-h-[calc(100vh_-_80px)] items-center justify-center py-4">
+        <HouseholdInitialCreate />
+      </div>
+    )
+
+  return (
+    <div>
+      {households.map((household) => (
+        <div key={household.id}>{household.id}</div>
+      ))}
+    </div>
+  )
+}
