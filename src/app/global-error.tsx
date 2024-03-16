22d35c6d7e42ef60bd1d70f34662335af14bddca
David Khierl <dkrgidor@gmail.com>
2024-03-16 22:22:07 +0400

    Add new GlobalError component
    A new file 'global-error.tsx' has been added, introducing a GlobalError component. This component displays a clear message and a "Try again" button whenever an error occurs, improving error handling and user experience.

---
+++ src/app/global-error.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body>
+        <div className="container flex h-screen flex-col items-center justify-center gap-2.5">
+          <h2>Something went wrong!</h2>
+          <Button onClick={() => reset()}>Try again</Button>
+        </div>
+      </body>
+    </html>
+  )
+}
