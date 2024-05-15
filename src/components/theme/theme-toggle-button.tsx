dfc46e8c78c9dcbb4e366f29059f556535a95d4c
David Khierl <dkrgidor@gmail.com>
2024-05-15 21:55:43 +0400

    Add theme toggle button component
    A new component for toggling between light, dark, and system themes has been implemented. It allows users to switch themes through a dropdown menu and is created in the file 'theme-toggle-button.tsx' under '/src/components/theme/'.

---
+++ src/components/theme/theme-toggle-button.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Button, ButtonProps } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Monitor, Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { forwardRef } from 'react'
+
+export const ThemeToggleButton = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  const { setTheme, theme } = useTheme()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button ref={ref} variant="secondary" size="icon" {...props}>
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>{' '}
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          <Sun className="mr-2 h-4 w-4" />
+          <span>Light</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          <Moon className="mr-2 h-4 w-4" />
+          <span>Dark</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          <Monitor className="mr-2 h-4 w-4" />
+          <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+})
+ThemeToggleButton.displayName = 'ThemeToggleButton'
