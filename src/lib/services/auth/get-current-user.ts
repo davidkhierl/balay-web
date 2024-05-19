450d9402b9acb3040d1911b06c6cf01f213a0b3f
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:01:45 +0400

    Refactor `getCurrentUser` function and improve error handling
    This commit focuses on the `getCurrentUser` function in the auth service, refining the return type and improving error handling. Instead of returning a generic error when failing to get a user, now it throws an `UnauthorizedError`, providing clearer context. The return type of `getCurrentUser` has been updated to return a `Promise<User>` for more consistency and comprehension.

--- src/lib/services/auth/get-current-user.ts
+++ src/lib/services/auth/get-current-user.ts
@@ -1,13 +1,15 @@
 import { getAuthUser } from '@/lib/services/auth/get-auth-user'
+import { UnauthorizedError } from '@/lib/services/error/unauthorized-error'
 import { getUser } from '@/lib/services/user/get-user'
+import { User } from '@/types/models.types'
 import { SupabaseClientDatabase } from '@/types/supabase.types'
 
-export async function getCurrentUser(supabase: SupabaseClientDatabase) {
+export async function getCurrentUser(supabase: SupabaseClientDatabase): Promise<User> {
   const authUser = await getAuthUser(supabase)
 
   const user = await getUser(authUser.id, supabase)
 
-  if (!user) throw new Error('Failed to get current user')
+  if (!user) throw new UnauthorizedError()
 
   return user
 }
