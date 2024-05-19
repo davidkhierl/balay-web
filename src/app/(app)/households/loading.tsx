e9a34a74598000da3bc1c341d948f6af63cac64a
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:24:42 +0400

    Add HouseholdsLoading component
    This update includes a new component for loading households data. The HouseholdsLoading component displays a loading message in a styled div container until the households data is fully loaded.

---
+++ src/app/(app)/households/loading.tsx
@@ -0,0 +1,7 @@
+export default function HouseholdsLoading() {
+  return (
+    <div className="container flex min-h-[calc(100dvh_-_64px)] items-center justify-center">
+      <p className="text-2xl">Households Loading</p>
+    </div>
+  )
+}
