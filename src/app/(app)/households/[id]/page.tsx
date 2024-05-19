75b70ab6cf92cbdbbe430ac3d3547193216dd8ba
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:25:18 +0400

    Add new HouseholdPage component
    This commit introduces a new HouseholdPage component. The component fetches household data from Supabase using the provided household ID and displays the household name. If the household data does not exist, the user is redirected to a not found page.

---
+++ src/app/(app)/households/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { getHousehold } from '@/lib/services/household/get-household'
+import { createClient } from '@/lib/supabase/server'
+import { notFound } from 'next/navigation'
+
+export default async function HouseholdPage({ params }: { params: { id: string } }) {
+  const supabase = createClient()
+  const { data: household } = await getHousehold(params.id, supabase)
+
+  if (!household) notFound()
+
+  return (
+    <div>
+      {household.name}
+      <div className="container flex gap-5"></div>
+    </div>
+  )
+}
