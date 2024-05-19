354bbfd6174f439ffef800add69283bebc2f1c9c
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:10:04 +0400

    Remove redundant Supabase client creation in AuthLogoutButton
    In the 'auth-logout-button.tsx' component, the creation of a Supabase client on each logout event is no longer necessary. The 'logout' action imported from 'logout.actions' already handles this, improving performance and reducing redundancy.

--- src/components/auth/auth-logout-button.tsx
+++ src/components/auth/auth-logout-button.tsx
@@ -1,14 +1,12 @@
 'use client'
 
 import { logout } from '@/actions/auth/logout.actions'
-import { createClient } from '@/lib/supabase/client'
 import { ButtonHTMLAttributes, forwardRef, MouseEventHandler } from 'react'
 
 export const AuthLogoutButton = forwardRef<
   HTMLButtonElement,
   ButtonHTMLAttributes<HTMLButtonElement>
 >(({ children, ...props }, ref) => {
-  const supabase = createClient()
   const handleOnClick: MouseEventHandler<HTMLButtonElement> = async (_event) => {
     await logout()
   }
