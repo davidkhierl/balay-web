7c2c7aba76c99f90c088c45ec85e978eed4ccd6d
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:09:11 +0400

    Overhaul AuthUserMenu component
    This commit significantly refactors the AuthUserMenu component. Notable changes include introducing new interface AuthUserMenuProps, and replacing former UI elements with more intricate new ones such as Dropdown Menu and UserAvatar. This enables a more interactive user interface within the component offering logout button, profile avatar and link to account page within the dropdown menu.

--- src/components/auth/auth-user-menu.tsx
+++ src/components/auth/auth-user-menu.tsx
@@ -1,23 +1,53 @@
+import { AuthLogoutButton } from '@/components/auth/auth-logout-button'
+import { AuthUserAvatar } from '@/components/auth/auth-user-avatar'
+import { AuthUserProfileAvatar } from '@/components/auth/auth-user-profile-avatar'
+import { ThemeDropdownRadioGroup } from '@/components/theme/theme-dropdown-menu-radio-group'
 import { Button } from '@/components/ui/button'
-import { UserAvatar } from '@/components/user/user-avatar'
-import { createClient } from '@/lib/supabase/server'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils/class-name'
+import { ChevronRight } from 'lucide-react'
 import Link from 'next/link'
 
-async function AuthUserMenu() {
-  const supabase = createClient()
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  if (!user)
-    return (
-      <Button asChild>
-        <Link href="/login">Log in</Link>
-      </Button>
-    )
-
-  return <UserAvatar />
+export interface AuthUserMenuProps {
+  className?: string
 }
 
-export { AuthUserMenu }
+export async function AuthUserMenu({ className }: AuthUserMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className={cn('h-14 w-14 rounded-full p-0', className)} variant="ghost">
+          <AuthUserAvatar className="h-full w-full" />
+          <span className="sr-only">user profile</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent className="min-w-72">
+          <DropdownMenuLabel>Account</DropdownMenuLabel>
+          <DropdownMenuItem asChild>
+            <Link href="/account" className="flex items-center justify-between gap-2.5">
+              <AuthUserProfileAvatar className="items-center" />
+              <ChevronRight className="h-5 w-5" />
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <ThemeDropdownRadioGroup />
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <AuthLogoutButton className="inline-flex w-full items-center gap-2">
+              Logout
+            </AuthLogoutButton>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
+  )
+}
