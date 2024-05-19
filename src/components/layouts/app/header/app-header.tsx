6ede1c57fd691586c775a692b6b99f76cac9682f
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:17:54 +0400

    Refactor AppHeader component structure and styles
    The AppHeader component file has been moved to a new folder and restructured to support mobile navigation menu. Along with this, certain styles such as the header height and border have been adjusted for better performance and appearance on multiple devices.

---
+++ src/components/layouts/app/header/app-header.tsx
@@ -0,0 +1,36 @@
+import BalayLogo from '@/assets/icons/balay-logo.svg'
+import { AuthUserMenu } from '@/components/auth/auth-user-menu'
+import { MobileMenu } from '@/components/layouts/app/header/mobile-menu/mobile-menu'
+import { navigationLinkItems } from '@/components/layouts/app/header/navigation-link-items'
+import { NavigationLinks } from '@/components/layouts/app/header/navigation-links'
+import { cn } from '@/lib/utils/class-name'
+import Link from 'next/link'
+
+export interface HeaderProps {
+  className?: string
+}
+
+export function AppHeader({ className }: HeaderProps) {
+  return (
+    <header className={cn('relative h-16', className)}>
+      <div className="container flex h-full max-w-full items-center justify-between gap-10 border-b border-neutral-200 px-2 dark:border-neutral-800 md:px-8">
+        <Link
+          href="/"
+          className="shrink-0 text-xl font-bold text-neutral-900 dark:text-neutral-200">
+          <BalayLogo className="h-10" />
+          <span className="sr-only">Balay home page</span>
+        </Link>
+
+        <div className="flex items-center justify-between md:flex-1">
+          <div>
+            <NavigationLinks className="hidden md:flex" items={navigationLinkItems} />
+            <MobileMenu navigationLinks={navigationLinkItems} />
+          </div>
+          <div className="hidden items-center gap-2.5 md:flex">
+            <AuthUserMenu className="h-10 w-10" />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
