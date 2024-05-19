05ea192dd3a3e693e8823c2705783e354af903a2
David Khierl <dkrgidor@gmail.com>
2024-05-20 00:24:30 +0400

    Update loading screen with Balay logo animation
    The loading screen is improved by incorporating the Balay logo animation. The previous placeholder text "loading.." has been replaced with the animated logo for a more engaging user experience during app loading times. The styling and layout of the loading screen have also been updated for better alignment and presentation.

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -1,3 +1,11 @@
+import BalayLogo from '@/assets/icons/balay-logo.svg'
+
 export default function AppLoading() {
-  return <div>loading..</div>
+  return (
+    <div className="container flex h-[calc(100dvh_-_64px)] flex-col items-center justify-center py-8">
+      <div className="mb-5 w-full max-w-md">
+        <BalayLogo className="animate-pulse" />
+      </div>
+    </div>
+  )
 }
