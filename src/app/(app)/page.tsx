32e853e5c36d06f5c7e7c8450a797fb6adc00cd6
David Khierl <dkrgidor@gmail.com>
2024-05-20 21:06:30 +0400

    Refactor HomePage's layout container
    Removed redundancy in HomePage by getting rid of an unnecessary div layer. This simplifies the code structure and keeps the same appearance and functionality intact.

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,18 +5,16 @@ import Link from 'next/link'
 
 export default async function HomePage() {
   return (
-    <div className="h-[calc(100dvh_-_64px)] py-8">
-      <div className="container flex h-full flex-col items-center justify-center rounded-lg bg-neutral-50 shadow-md">
-        <h1 className="text-center text-lg md:text-3xl">Making Shared Living a Breeze</h1>
-        <BalayLogo className="w-full max-w-lg" />
-        <Button
-          asChild
-          className="mt-5"
-          icon={<ChevronRight className="h-5 w-5" />}
-          iconPlacement="right">
-          <Link href="/households">Your Households</Link>
-        </Button>
-      </div>
+    <div className="container flex h-[calc(100dvh_-_64px)] flex-col items-center justify-center py-8">
+      <h1 className="text-center text-lg md:text-3xl">Making Shared Living a Breeze</h1>
+      <BalayLogo className="w-full max-w-lg" />
+      <Button
+        asChild
+        className="mt-5"
+        icon={<ChevronRight className="h-5 w-5" />}
+        iconPlacement="right">
+        <Link href="/households">Your Households</Link>
+      </Button>
     </div>
   )
 }
